10c47253555c1c9efe07b6fb108ea2099c463882
ryang <ryangutierrez@billericak12.com>
2019-10-25 10:15:45 -0400

    popo

---
+++ line/src/Circle.java
@@ -0,0 +1,25 @@
+public class Circle{
+    private double r;
+    private Circle c1;
+
+    public Circle()
+    {
+        r = 1;
+    }
+    public Circle(double radius)
+    {
+        r = radius;
+    }
+    public double Area()
+    {
+        double a = Math.PI*Math.pow(r, 2);
+        return a;
+    }
+    public double Circumference()
+    {
+        double c =2*r*Math.PI;
+        return c;
+    }
+
+
+}
